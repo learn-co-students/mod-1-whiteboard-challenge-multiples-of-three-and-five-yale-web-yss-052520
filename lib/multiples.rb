37a416e1fdf8346da06b443be20086df30db40a1
chad <chad.palmer@yale.edu>
2020-06-10 14:39:58 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -5,3 +5,14 @@ end
 def sum_multiples(limit)
     collect_multiples(limit).sum
 end
+
+###
+
+# def sum_multiples(limit)
+#     true_limit = limit - 1
+#     multiple_adder(true_limit, 3) + multiple_adder(true_limit, 5) - multiple_adder(true_limit, 15)
+# end
+
+# def multiple_adder(true_limit, multiple)
+#     ((true_limit / multiple) * ((true_limit / multiple) + 1) * multiple) / 2
+# end
