844f1ca979bd09b90982e224d4f064747556df26
alexisteh <wenhui.teh23@gmail.com>
2020-06-10 12:45:40 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    array = [] 
+    max = limit -1 
+    (1..max).each do |num| 
+        if num %3 == 0 || num%5 == 0 
+            array << num 
+        end 
+    end 
+    return array  
+end 
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end 
