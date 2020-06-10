ace0f94e4988b87b8fb9dbe375d46c741c9d73e3
Joao Cardoso <joao.cardoso@yale.edu>
2020-06-10 13:49:03 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,8 +1,11 @@
 # Enter your procedural solution here!
+
+#collects all multiples of 3 or 5 in a range from 1 up until given limit
 def collect_multiples(limit)
     (1...limit).select{|number| number%3 == 0 or number%5 == 0}
 end
 
+    #sums all the multiples collected by collect multiples within given limit
 def sum_multiples(limit)
     collect_multiples(limit).sum
 end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,14 +1,18 @@
 # Enter your object-oriented solution here!
 class Multiples
-    attr_accessor :limit
+    attr_reader :limit
+
+    #initializes instance with a limit
     def initialize(limit)
         @limit = limit
     end
 
+    #collects all multiples of 3 or 5 in a range from 1 up until instance's limit
     def collect_multiples
         (1...@limit).select{|number| number%3 == 0 or number%5 == 0}
     end
 
+    #sums all the multiples collected by collect multiples
     def sum_multiples
         collect_multiples.sum
     end
