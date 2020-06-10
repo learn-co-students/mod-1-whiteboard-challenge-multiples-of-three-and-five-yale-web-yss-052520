8dc9fac10ffc153a0a7e1efca623f6ca327c11bb
robertalopez <roblop1234@yahoo.com>
2020-06-10 13:18:56 -0700

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+    def collect_multiples(limit)
+        (3..(limit - 1)).select {|mult| mult%3 == 0 || mult%5 == 0}
+    end
+
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum 
+end 
+
+
+
+
