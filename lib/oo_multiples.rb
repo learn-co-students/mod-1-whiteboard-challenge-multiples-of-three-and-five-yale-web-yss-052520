6465ea45a726f55a1654d5592f0020dfcdcfbe08
Dennis <Dennis.portillo@yale.edu>
2020-06-10 10:54:35 -0700

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_reader :num
+    def initialize(num)
+        @num = num
+    end
+
+    def collect_multiples
+        array = []
+        n = 1
+        while n < num do
+            if n % 3 == 0 || n % 5 == 0
+                array << n
+            end
+            n += 1
+        end
+        array
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+    end
+end
