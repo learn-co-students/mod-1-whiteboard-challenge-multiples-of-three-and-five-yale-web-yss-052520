2369fe77e09bdb15c620ea72a9b4d0cc2f8ae5bd
justin_james <jjames11799@gmail.com>
2020-06-10 14:04:21 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,20 @@
 # Enter your procedural solution here!
+# def collect_multiples(limit)
+#     naturals = []
+#     new_limit = limit + 1
+#     new_limit.times do |num|
+#         if num % 3 == 0 || num % 5 == 0
+#             naturals << num
+#         end
+#     end
+#     naturals 
+# end
+
+def collect_multiples(limit)
+    (1...limit).to_a.select{|i| i % 3 == 0 || i % 5 == 0}
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum 
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_accessor :limit 
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples 
+        (1...self.limit).to_a.select{|i| i % 3 == 0 || i % 5 == 0}
+    end
+
+    def sum_multiples
+        collect_multiples.sum 
+    end
+end
