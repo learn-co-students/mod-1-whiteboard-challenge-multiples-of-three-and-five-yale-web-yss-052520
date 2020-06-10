82a54afe190320cc5294a36444131cbdbcf3b1cb
s-dney <littlebirdsword@gmail.com>
2020-06-10 10:44:59 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+require 'pry'
+
+def collect_multiples(limit)
+
+  all = Array.new(limit-1) {|i| i+1}
+
+  all.select!{|num| num % 3 == 0 || num % 5 == 0}
+
+
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).sum
+end
