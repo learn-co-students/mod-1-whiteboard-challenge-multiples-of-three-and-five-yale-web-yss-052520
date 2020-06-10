ebf684a53d3063d7ad67ef0e34ee917a8e9c4f0c
ZIming Mao <ziming.mao@yale.edu>
2020-06-10 14:06:28 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,11 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    (1...limit).to_a.select{
+        |num| num % 3 == 0 || num % 5 == 0
+    }
+    # we can remove the "to_a" as well
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
