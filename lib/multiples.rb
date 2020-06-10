50e57497fa2cd7df55fac0f85fcccc3ff7d39943
Jonkavaliauskas <jonas.kavaliauskas@yale.edu>
2020-06-10 20:57:17 +0300

    Done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    l = limit - 1
+    multiples = (1..l).to_a
+    multiples.select{|m| m % 3 == 0 || m % 5 == 0}
+end    
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end    
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,19 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_reader :limit, :multiples
+
+    def initialize(limit)
+        @limit = limit - 1
+        @multiples = collect_multiples
+    end
+    
+    def collect_multiples
+        multiples1 = (1..limit).to_a
+        multiples1.select{|m| m % 3 == 0 || m % 5 == 0}
+    end    
+    
+    def sum_multiples
+        collect_multiples.sum
+    end    
+
+end    
