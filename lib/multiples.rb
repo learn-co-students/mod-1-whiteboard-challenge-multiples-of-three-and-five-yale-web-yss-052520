70ba4d77ce44a6aaf62f1edc773ce189c2903e4a
Eric Ou <eric.ou@yale.edu>
2020-06-10 10:39:30 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,8 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+    [*3..num - 1].select{|val| val%3 == 0 || val%5 == 0}
+end
+
+def sum_multiples(num)
+    collect_multiples(num).sum()
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,14 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_reader :num
+    def initialize(num)
+        @num = num
+    end
+    def collect_multiples()
+        [*3..num - 1].select{|val| val%3 == 0 || val%5 == 0}
+    end
+    
+    def sum_multiples()
+        collect_multiples().sum()
+    end
+end
