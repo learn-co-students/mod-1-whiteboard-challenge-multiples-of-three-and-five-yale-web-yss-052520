5b273e33e50f462b1948a72d2420b41f2630285e
edensongjbs <edensongjbs@gmail.com>
2020-06-11 13:35:05 -0400

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+    attr_reader :limit, :all
+    def initialize(limit)
+        @limit=limit
+        @all=[]
+    end
+    def collect_multiples
+     num = 1
+        while (num < limit)
+            if num%3==0  || num%5==0
+               all << num
+         end
+         num+=1
+        end
+        all
+    end
+
+    def sum_multiples
+        collect_multiples.reduce(0) {|sum, num| sum+=num}
+    end
+end
