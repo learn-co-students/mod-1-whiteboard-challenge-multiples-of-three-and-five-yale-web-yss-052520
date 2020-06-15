1c77b525497e47e7898558b8765a1f9efd695de6
davidyu7 <david.yu@yale.edu>
2020-06-14 22:51:10 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,26 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    (1...limit).select{|i| i % 3 == 0 || i % 5 == 0}
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
+
+class Multiples
+
+    @multiples = []
+
+    def initialize(limit)
+        @multiples = (1...limit).select{|i| i % 3 == 0 || i % 5 == 0}
+    end
+
+    def collect_multiples
+        @multiples
+    end
+
+    def sum_multiples
+        self.collect_multiples.sum
+    end
+end
